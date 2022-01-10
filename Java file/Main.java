341f740a9a70ecc581928d5ea324eb2228c8ed99
JoeIrving1992 <97188965+JoeIrving1992@users.noreply.github.com>
2022-01-10 23:35:22 +0000

    Add files via upload

---
+++ Java file/Main.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.io.IOException;
+public class Main
+{
+       public static void main(String[] args) throws IOException {
+        Controller controller = new Controller();
+        controller.showMenu();
+    }
+}
