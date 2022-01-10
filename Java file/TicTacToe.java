be7394904bc842ab0b058d9afcf3bb173946117a
JoeIrving1992 <97188965+JoeIrving1992@users.noreply.github.com>
2022-01-10 20:42:21 +0000

    Add files via upload

---
+++ Java file/TicTacToe.java
@@ -0,0 +1,36 @@
+import java.nio.file.FileSystemNotFoundException;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		
+		char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+				{'-', '+', '-', '+', '-'},
+				{' ', '|', ' ', '|', ' '},
+				{'-', '+', '-', '+', '-'},
+				{' ', '|', ' ', '|', ' '}};
+		
+		printGameBoard(gameBoard);
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your Placement (1-9):");
+		int pos = scan.nextInt();
+		
+		System.out.println(pos);
+	}
+		
+		
+	
+	public static void printGameBoard(char [][] gameBoard) {
+		for(char[] row : gameBoard) {
+			for(char c : row) {
+				System.out.print(c);
+			}
+			System.out.println();
+		}
+	
+
+	}
+		
+	}
---
+++ Java file/ZombieKiller.java
@@ -0,0 +1,27 @@
+
+public class ZombieKiller {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to our zombie killer program!");
+		
+		String backpack[] = {"Shotgun", "Assault Rifle", "Sniper", "Pistol", "Knife"};
+		String zombies[] = {"Fast Zombie","Close-Range Zombie", "Slow Mid-Range Zombie", "Mid-Range Zombie", "Slow Long-Range Zombie"};
+			
+			System.out.println("Backpack Items:");
+			System.out.println(backpack[0]);
+			System.out.println(backpack[1]);
+			System.out.println(backpack[2]);
+			System.out.println(backpack[3]);
+			System.out.println(backpack[4]);
+			
+			System.out.println("The Zombies:");
+			System.out.println(zombies[0]);
+			System.out.println(zombies[1]);
+			System.out.println(zombies[2]);
+			System.out.println(zombies[3]);
+			System.out.println(zombies[4]);
+			
+			
+		}
+		
+	}
